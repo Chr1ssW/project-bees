a0094b101f985dd70759a3e1f29d6b91b298ff3a
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2020-11-11 20:09:05 +0100

    Sign in added

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -8,3 +8,13 @@ function closeNav()
     document.getElementById("sidebar").style.transform = "translateX(-45vh)";
 
 }
+
+function openLogin()
+{
+    document.getElementById("login-container").style.display = "flex";
+}
+
+function closeLogin()
+{
+    document.getElementById("login-container").style.display = "none";
+}
