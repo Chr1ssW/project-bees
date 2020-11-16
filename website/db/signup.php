c0df6a55c8aa3163e2da271b483abfdc015a4fd0
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-11-16 10:40:44 +0100

    Database

--- website/db/signup.php
+++ website/db/signup.php
@@ -8,6 +8,7 @@ $HostName = "eu-cdbr-west-03.cleardb.net";
 $DBName = "heroku_574ab15869a35be";
 
 $conn = mysqli_connect($HostName, $username, $passwd, $DBName);
+
 if(!$conn)
 {
     die("Connection to the database failed" . mysqli_connect_error());
@@ -42,3 +43,4 @@ if(isset($_POST['signupForm']))
         echo "User created successfully";
     }
 }
+?>
