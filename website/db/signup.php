93fbf37814fd2f1dea59f939c532f691d09e8532
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-11-17 13:59:32 +0100

    Displaying errors

--- website/db/signup.php
+++ website/db/signup.php
@@ -74,9 +74,14 @@ if(isset($_POST['signup']))
     }
     else
     {
-        foreach ($errors as $error) {
-            echo "$error <br>";
-        }
+            ?><?php  if (count($errors) > 0) : ?>
+                <div class="invalid-response">
+                    <?php foreach ($errors as $error) : ?>
+                        <p><?php echo $error ?></p>
+                    <?php endforeach ?>
+                </div>
+            <?php  endif ?>
+<?php
     }
 }
 else
