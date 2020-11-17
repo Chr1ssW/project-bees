78a5f93f16b3dd3a71139893401cd988060eb402
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-11-17 20:19:37 +0100

    Update index.php

--- website/html/index.php
+++ website/html/index.php
@@ -69,13 +69,13 @@ require_once("../db/connect.php");
         </div>
     </div>
     <div class="popup-screen" id="signup-container">
-        <div class="popup-form">
+        <div id="popup-form">
             <a href="javascript:void(0)" class="closebtn" onclick="closeSignup()">&times;</a>
             <div class="loginPic">
                 <img src="../resources/img/account.png" alt="Account">
             </div>
             <!--Need to change it to post in the future-->
-            <form method="POST" action="index.php#signup-container" id="signupForm">
+            <form method="POST" action="index.php" id="signupForm" target=_blank onsubmit="openSignup()">
                 <input type="text" placeholder="Username" name="userNameIn">
                 <input type="text" placeholder="Email address" name="emailAddressIn">
                 <input type="password" placeholder="Password" name="passwordIn">
