bc7237f0a8ffd76c9ba1302caa0424957052a7c2
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-11-17 15:39:39 +0100

    Update errors.php

--- website/db/errors.php
+++ website/db/errors.php
@@ -1,8 +1,8 @@
 <?php
-if (count($errors) > 0) : ?>
-    <div class="invalid-response">
-        <?php foreach ($errors as $error) : ?>
-            <p><?php echo $error ?></p>
-        <?php endforeach ?>
-    </div>
-<?php  endif ?>
+if (count($errors) > 0)
+{
+    foreach($errors as $error)
+    {
+        echo $error <br>;
+    }
+}
