0536f9469884d193437361a5949cba5f06625364
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2020-11-18 21:07:35 +0100

    reference to js changed

--- website/html/beehivesMap.php
+++ website/html/beehivesMap.php
@@ -33,13 +33,14 @@
             <div id = "myMap">
 
             </div>
-            <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-             integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-            crossorigin=""></script>
-            <script src="map.js"></script>
+            
         </main>
         <footer></footer>
     </div>
     <script src="../js/scripts.js"></script> 
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+             integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+            crossorigin=""></script>
+            <script src="../js/map.js"></script>
 </body>
 </html>
