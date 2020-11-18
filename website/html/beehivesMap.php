227ee761f164dbecc9f3d3a25c4c21c3022544d0
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2020-11-18 10:25:01 +0100

    Sidebar and popup taken out and replaced

---
+++ website/html/beehivesMap.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin="">
+        //JavaScript code goes here? 
+    </script>
+    <title>Beehive Monitoring System</title>
+</head>
+<body>
+    <?php
+    require_once("sidebarAndPopup.html");
+    ?>
+    <div id="main">
+        <header>
+            <nav>
+                <span>
+                    <a href="javascript:void(0)" onclick="openNav()">
+                        <img src="../resources/img/menu.png" alt="menu">
+                    </a>
+                    <h2>Beehive Monitoring System</h2>
+                </span>
+                <span>
+                    <button class="signup-btn" type="button" onclick="openSignup()">Sign up</button>
+                    <button class="signin-btn" type="button" onclick="openLogin()">Sign in</button>
+                </span>
+                
+            </nav>
+        </header>
+        <main>
+            <div id = "map">
+                <p>test</p>
+            </div>
+        </main>
+        <footer></footer>
+    </div>
+    <script src="../js/scripts.js"></script> 
+</body>
+</html>
