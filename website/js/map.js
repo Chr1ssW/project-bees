379e8c9c33fca1f8ea022210fed4df2b85d96690
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2020-11-19 12:44:32 +0100

    change zoom in map

--- website/js/map.js
+++ website/js/map.js
@@ -1,6 +1,6 @@
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-let myMap = L.map('myMap').setView([52.77823, 6.91170], 13)
+let myMap = L.map('myMap').setView([52.77823, 6.91170], 18)
 
 L.tileLayer(tilesProvider, {
     maxZoom: 18,
