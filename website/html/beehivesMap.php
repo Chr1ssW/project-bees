82ab8ed52dc3bf4b5ba441941e4a62a9b47d1ec5
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2020-11-19 12:34:11 +0100

    remove text

--- website/html/beehivesMap.php
+++ website/html/beehivesMap.php
@@ -30,7 +30,6 @@
         </header>
         <main>
             <div id = "myMap">
-            <p>Test to see if it can be seen on webpage</p>
             </div>
         </main>
         <footer></footer>
