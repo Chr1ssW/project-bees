40330887e154b745963456f1b680f34a023a882c
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-12-06 16:03:31 +0100

    Database adjustment

--- website/db/updateHive.php
+++ website/db/updateHive.php
@@ -3,13 +3,12 @@ require("../db/connect.php");
 if(isset($_POST["updateHive"]))
 {
     $beeHiveLocation = htmlentities($_POST['editBeehiveLocation']);
-    $userID = $_SESSION['userID'];
     if (empty($beeHiveLocation)) {
         header("Location: ../html/userBeeHives.php?=empty&input");
     } else {
         $sqlInsertBeeHive = "UPDATE beehive SET location = ? WHERE sensorID = ?";
         if ($stmtInsertBeeHive = mysqli_prepare($conn, $sqlInsertBeeHive)) {
-            mysqli_stmt_bind_param($stmtInsertBeeHive, 'ss', $beeHiveLocation, $beeID);
+            mysqli_stmt_bind_param($stmtInsertBeeHive, 'si', $beeHiveLocation, $beeID);
             if (mysqli_stmt_execute($stmtInsertBeeHive)) {
                 header("Location: ../html/userBeeHives.php?=beehive%updated");
             }
