16f4db100135052780741446c835817291941d08
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-12-06 15:54:58 +0100

    Bug fix

--- website/db/updateHive.php
+++ website/db/updateHive.php
@@ -9,12 +9,12 @@ if(isset($_POST["updateHive"]))
         $sqlInsertBeeHive = "UPDATE beehive SET location = ? WHERE sensorID = ?";
         if ($stmtInsertBeeHive = mysqli_prepare($conn, $sqlInsertBeeHive)) {
             mysqli_stmt_bind_param($stmtInsertBeeHive, 'ss', $beeHiveLocation, $beeID);
-            if (mysqli_stmt_execute($stmtInsertBeeHive) == FALSE) {
-                echo mysqli_error($conn);
+            if (mysqli_stmt_execute($stmtInsertBeeHive)) {
+                header("Location: ../html/userBeeHives.php?=beehive%updated");
             }
             else
             {
-                header("Location: ../html/userBeeHives.php?=beehive%updated");
+                mysqli_error($conn);
             }
             mysqli_stmt_close($stmtInsertBeeHive);
         }
