4aa3f0464ec7aa91e486c7318bfaea6ed5138348
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-12-06 17:20:16 +0100

    Update userBeeHives.php

--- website/html/userBeeHives.php
+++ website/html/userBeeHives.php
@@ -97,7 +97,26 @@ if(!isset($_SESSION['loggedin'])){
                         }
                         mysqli_stmt_close($stmtSelect);
                     }
-                    include("../db/updateHive.php");
+                    if(isset($_POST["updateHive"]))
+                    {
+                        $beeHiveLocation = htmlentities($_POST['editBeehiveLocation']);
+                        if (empty($beeHiveLocation)) {
+                            header("Location: ../html/userBeeHives.php?=empty&input");
+                        } else {
+                            $sqlInsertBeeHive = "UPDATE beehive SET location = ? WHERE sensorID = ?";
+                            if ($stmtInsertBeeHive = mysqli_prepare($conn, $sqlInsertBeeHive)) {
+                                mysqli_stmt_bind_param($stmtInsertBeeHive, 'ss', $beeHiveLocation, $beeID);
+                                if (mysqli_stmt_execute($stmtInsertBeeHive)) {
+                                    header("Location: ../html/userBeeHives.php?=beehive%updated");
+                                }
+                                else
+                                {
+                                    mysqli_error($conn);
+                                }
+                                mysqli_stmt_close($stmtInsertBeeHive);
+                            }
+                        }
+                    }
                     ?>
                 </div>
             </main>
