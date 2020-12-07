9c0dcbb8e50466a900ce23ea059dcda918928258
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-12-07 16:41:41 +0100

    Update userBeeHives.php

--- website/html/userBeeHives.php
+++ website/html/userBeeHives.php
@@ -40,7 +40,7 @@ require_once("sidebarAndPopup.php");
             <div class="change-form">
                 <form method="POST" action="../db/updateHive.php" id="editBeeHive">
                     <input type="text" name="editBeehiveLocation" placeholder="Beehive location" id="locationNew">
-                    <p id="beehive-id" name="id"></p>
+                    <input type="hidden" name="id" value="beehive-id" id="beehive-id">
                 </form>
             </div>
         </div>
