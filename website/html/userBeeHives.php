4bbb0a559f95bf14030175cee9453e93c80aa910
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-12-07 11:21:31 +0100

    Bug fix

--- website/html/userBeeHives.php
+++ website/html/userBeeHives.php
@@ -97,6 +97,23 @@ if(!isset($_SESSION['loggedin'])){
                         }
                         mysqli_stmt_close($stmtSelect);
                     }
+                    if(isset($_POST["updateHive"]))
+                    {
+                        $beeHiveLocation = htmlentities($_POST['editBeehiveLocation']);
+                        if (empty($beeHiveLocation)) {
+                            header("Location: ../html/userBeeHives.php?=empty&input");
+                        } else {
+                            $sqlUpdateBeeHive = "UPDATE beehive SET location = ? WHERE sensorID = ?";
+                            if ($stmtUpdateBeeHive = mysqli_prepare($conn, $sqlUpdateBeeHive)) {
+                                mysqli_stmt_bind_param($stmtUpdateBeeHive, 'ss', $beeHiveLocation, $beeID);
+                                if (mysqli_stmt_execute($stmtUpdateBeeHive) == FALSE) {
+                                    echo mysqli_error($conn);
+                                }
+                                mysqli_stmt_close($stmtUpdateBeeHive);
+                                header("Location: ../html/userBeeHives.php?=beehive%updated");
+                            }
+                        }
+                    }
                     ?>
                 </div>
             </main>
