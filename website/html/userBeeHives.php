d5f0af99198cd22692792a829334f58d5dbdc035
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2020-12-07 11:08:49 +0100

    Bug fix

--- website/html/userBeeHives.php
+++ website/html/userBeeHives.php
@@ -103,17 +103,14 @@ if(!isset($_SESSION['loggedin'])){
                         if (empty($beeHiveLocation)) {
                             header("Location: ../html/userBeeHives.php?=empty&input");
                         } else {
-                            $sqlInsertBeeHive = "UPDATE beehive SET location = '?' WHERE sensorID = ?";
-                            if ($stmtInsertBeeHive = mysqli_prepare($conn, $sqlInsertBeeHive)) {
-                                mysqli_stmt_bind_param($stmtInsertBeeHive, 'ss', $beeHiveLocation, $beeID);
-                                if (mysqli_stmt_execute($stmtInsertBeeHive)) {
-                                    header("Location: ../html/userBeeHives.php?=beehive%updated");
+                            $sqlUpdateBeeHive = "UPDATE beehive SET location = '?' WHERE sensorID = '?'";
+                            if ($stmtUpdateBeeHive = mysqli_prepare($conn, $sqlUpdateBeeHive)) {
+                                mysqli_stmt_bind_param($stmtUpdateBeeHive, 'ss', $beeHiveLocation, $beeID);
+                                if (mysqli_stmt_execute($stmtUpdateBeeHive) == FALSE) {
+                                    echo mysqli_error($conn);
                                 }
-                                else
-                                {
-                                    mysqli_error($conn);
-                                }
-                                mysqli_stmt_close($stmtInsertBeeHive);
+                                mysqli_stmt_close($stmtUpdateBeeHive);
+                                header("Location: ../html/userBeeHives.php?=beehive%updated");
                             }
                         }
                     }
