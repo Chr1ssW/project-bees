ccbacf489dbbfcc9017519d01b15286ffcd6cb94
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2020-12-16 15:22:44 +0100

    All charts added
    1. Humidity chart added
    2. Current information added

--- website/html/dashboard.php
+++ website/html/dashboard.php
@@ -38,110 +38,256 @@ require_once("../db/connect.php");
         <title>Beehive Monitoring System</title>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
-        google.charts.load('current', {'packages':['corechart']});
-        google.charts.setOnLoadCallback(drawChart);
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
 
-        <?php
-            //Selecting temperature information
-            $sqlSelect = "SELECT  date(timeStamp), internalTemp, externalTemp 
-            FROM beehive_data 
-            WHERE timeStamp >= ? 
-                AND timeStamp <= ?
-                AND sensorID = ?
-            GROUP BY date(timeStamp) 
-            ORDER BY timeStamp"; 
-
-            if ($stmtSelect = mysqli_prepare($conn, $sqlSelect)) {
-            mysqli_stmt_bind_param($stmtSelect, 'sss',  $startDate, $endDate, $selectedHive);
-            $executeSelect = mysqli_stmt_execute($stmtSelect);
-            if ($executeSelect == FALSE) {
-            echo mysqli_error($conn);
-            }
-            mysqli_stmt_bind_result($stmtSelect, $timeStamp, $internalTemp, $externalTemp);
-            mysqli_stmt_store_result($stmtSelect);
-
-            }
-        ?>
-
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-            ['Date', 'External', 'Internal'],
             <?php
-                if (!mysqli_stmt_num_rows($stmtSelect) == 0){
-                    while (mysqli_stmt_fetch($stmtSelect)) {
-                        echo "['$timeStamp', $externalTemp, $internalTemp], ";
-                    }
+                //Selecting temperature information
+                $sqlSelect = "SELECT  date(timeStamp), internalTemp, externalTemp 
+                FROM beehive_data 
+                WHERE timeStamp >= ? 
+                    AND timeStamp <= ?
+                    AND sensorID = ?
+                GROUP BY date(timeStamp) 
+                ORDER BY timeStamp"; 
+
+                if ($stmtSelect = mysqli_prepare($conn, $sqlSelect)) {
+                mysqli_stmt_bind_param($stmtSelect, 'sss',  $startDate, $endDate, $selectedHive);
+                $executeSelect = mysqli_stmt_execute($stmtSelect);
+                if ($executeSelect == FALSE) {
+                echo mysqli_error($conn);
                 }
+                mysqli_stmt_bind_result($stmtSelect, $timeStamp, $internalTemp, $externalTemp);
+                mysqli_stmt_store_result($stmtSelect);
 
-                mysqli_stmt_close($stmtSelect);
+                }
             ?>
-            ]);
 
-            var options = {
-                title: 'Internal temperature',
-                backgroundColor: '#0B2638',
-                colors:['#EAB111','white'],
-                chartArea: {'width': '80%', 'height': '80%'},
-                hAxis: {
-                        title: 'Date',
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                ['Date', 'External', 'Internal'],
+                <?php
+                    if (!mysqli_stmt_num_rows($stmtSelect) == 0){
+                        while (mysqli_stmt_fetch($stmtSelect)) {
+                            echo "['$timeStamp', $externalTemp, $internalTemp], ";
+                        }
+                    }
+
+                    mysqli_stmt_close($stmtSelect);
+                ?>
+                ]);
+
+                var options = {
+                    title: 'Internal temperature',
+                    backgroundColor: '#0B2638',
+                    colors:['#EAB111','white'],
+                    chartArea: {'width': '80%', 'height': '70%'},
+                    hAxis: {
+                            title: 'Date',
+                            textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    vAxis: {
+                        minValue: 0,
                         textStyle: {
-                            color: '#ffffff'
-                        }
-                    },
-                vAxis: {
-                    minValue: 0,
-                    textStyle: {
-                            color: '#ffffff'
-                        }
-                    },
-                legend: {
-                    position: 'bottom',
-                    textStyle: {
-                            color: '#ffffff'
-                        }
-                    },
-                titleTextStyle: {
-                    color: '#ffffff'
-                }           
-            };
+                                color: '#ffffff'
+                            }
+                        },
+                    legend: {
+                        position: 'bottom',
+                        textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    titleTextStyle: {
+                        color: '#ffffff'
+                    }           
+                };
 
-            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
-        }
+                var chart = new google.visualization.AreaChart(document.getElementById('chart-1'));
+                chart.draw(data, options);
+            }
         </script>
         <script type="text/javascript">
-        google.charts.load('current', {'packages':['corechart']});
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            <?php
+                //Selecting weight information
+                $sqlSelect = "SELECT  date(timeStamp), weight 
+                FROM beehive_data 
+                WHERE timeStamp >= ? 
+                    AND timeStamp <= ?
+                    AND sensorID = ?
+                GROUP BY date(timeStamp) 
+                ORDER BY timeStamp"; 
+
+                if ($stmtSelect = mysqli_prepare($conn, $sqlSelect)) {
+                mysqli_stmt_bind_param($stmtSelect, 'sss',  $startDate, $endDate, $selectedHive);
+                $executeSelect = mysqli_stmt_execute($stmtSelect);
+                if ($executeSelect == FALSE) {
+                echo mysqli_error($conn);
+                }
+                mysqli_stmt_bind_result($stmtSelect, $timeStamp, $weight);
+                mysqli_stmt_store_result($stmtSelect);
+
+                }
+            ?>
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                ['Date', 'Weight'],
+                <?php
+                    if (!mysqli_stmt_num_rows($stmtSelect) == 0){
+                        while (mysqli_stmt_fetch($stmtSelect)) {
+                            echo "['$timeStamp', $weight], ";
+                        }
+                    }
+
+                    mysqli_stmt_close($stmtSelect);
+                ?>
+                ]);
+
+                var options = {
+                    title: 'Weight',
+                    backgroundColor: '#0B2638',
+                    colors:['#EAB111'],
+                    chartArea: {'width': '80%', 'height': '70%'},
+                    hAxis: {
+                            title: 'Date',
+                            textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    vAxis: {
+                        minValue: 0,
+                        textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    legend: {
+                        position: 'bottom',
+                        textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    titleTextStyle: {
+                        color: '#ffffff'
+                    }           
+                };
+
+                var chart = new google.visualization.AreaChart(document.getElementById('chart-2'));
+                chart.draw(data, options);
+            }
+        </script>
+        <script type="text/javascript">
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            <?php
+                //Selecting weight information
+                $sqlSelect = "SELECT  date(timeStamp), humidity 
+                FROM beehive_data 
+                WHERE timeStamp >= ? 
+                    AND timeStamp <= ?
+                    AND sensorID = ?
+                GROUP BY date(timeStamp) 
+                ORDER BY timeStamp"; 
+
+                if ($stmtSelect = mysqli_prepare($conn, $sqlSelect)) {
+                mysqli_stmt_bind_param($stmtSelect, 'sss',  $startDate, $endDate, $selectedHive);
+                $executeSelect = mysqli_stmt_execute($stmtSelect);
+                if ($executeSelect == FALSE) {
+                echo mysqli_error($conn);
+                }
+                mysqli_stmt_bind_result($stmtSelect, $timeStamp, $humidity);
+                mysqli_stmt_store_result($stmtSelect);
+
+                }
+            ?>
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                ['Date', 'Humidity'],
+                <?php
+                    if (!mysqli_stmt_num_rows($stmtSelect) == 0){
+                        while (mysqli_stmt_fetch($stmtSelect)) {
+                            echo "['$timeStamp', $humidity], ";
+                        }
+                    }
+
+                    mysqli_stmt_close($stmtSelect);
+                ?>
+                ]);
+
+                var options = {
+                    title: 'Humidity',
+                    backgroundColor: '#0B2638',
+                    colors:['#EAB111'],
+                    chartArea: {'width': '80%', 'height': '70%'},
+                    hAxis: {
+                            title: 'Date',
+                            textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    vAxis: {
+                        minValue: 0,
+                        textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    legend: {
+                        position: 'bottom',
+                        textStyle: {
+                                color: '#ffffff'
+                            }
+                        },
+                    titleTextStyle: {
+                        color: '#ffffff'
+                    }           
+                };
+
+                var chart = new google.visualization.AreaChart(document.getElementById('chart-3'));
+                chart.draw(data, options);
+            }
+        </script>
+        <script type="text/javascript">
+        google.charts.load('current', {'packages':['gauge']});
         google.charts.setOnLoadCallback(drawChart);
 
         <?php
-            //Selecting temperature information
-            $sqlSelect = "SELECT  date(timeStamp), weight 
-            FROM beehive_data 
-            WHERE timeStamp >= ? 
-                AND timeStamp <= ?
-                AND sensorID = ?
-            GROUP BY date(timeStamp) 
-            ORDER BY timeStamp"; 
+                //Selecting current information
+                $sqlSelect = "SELECT  internalTemp, externalTemp, humidity, weight
+                    FROM beehive_data 
+                    WHERE sensorID = ?
+                    ORDER BY timeStamp DESC
+                    LIMIT 1"; 
 
-            if ($stmtSelect = mysqli_prepare($conn, $sqlSelect)) {
-            mysqli_stmt_bind_param($stmtSelect, 'sss',  $startDate, $endDate, $selectedHive);
-            $executeSelect = mysqli_stmt_execute($stmtSelect);
-            if ($executeSelect == FALSE) {
-            echo mysqli_error($conn);
-            }
-            mysqli_stmt_bind_result($stmtSelect, $timeStamp, $weight);
-            mysqli_stmt_store_result($stmtSelect);
+                if ($stmtSelect = mysqli_prepare($conn, $sqlSelect)) {
+                    mysqli_stmt_bind_param($stmtSelect, 's', $selectedHive);
+                    $executeSelect = mysqli_stmt_execute($stmtSelect);
+                    if ($executeSelect == FALSE) {
+                        echo mysqli_error($conn);
+                    }
 
-            }
-        ?>
+                    mysqli_stmt_bind_result($stmtSelect, $internalTemp, $externalTemp, $humidity, $weight);
+                    mysqli_stmt_store_result($stmtSelect);
+                }
+            ?>
 
         function drawChart() {
+
             var data = google.visualization.arrayToDataTable([
-            ['Date', 'Weight'],
+            ['Label', 'Value'],
             <?php
                 if (!mysqli_stmt_num_rows($stmtSelect) == 0){
                     while (mysqli_stmt_fetch($stmtSelect)) {
-                        echo "['$timeStamp', $weight], ";
+                        echo "['Ext. temp.', $externalTemp], ";
+                        echo "['Int. temp.', $internalTemp], ";
+                        echo "['Humidity', $humidity], ";
+                        echo "['Weight', $weight], ";
                     }
                 }
 
@@ -150,34 +296,15 @@ require_once("../db/connect.php");
             ]);
 
             var options = {
-                title: 'Weight',
-                backgroundColor: '#0B2638',
-                colors:['#EAB111'],
-                chartArea: {'width': '80%', 'height': '80%'},
-                hAxis: {
-                        title: 'Date',
-                        textStyle: {
-                            color: '#ffffff'
-                        }
-                    },
-                vAxis: {
-                    minValue: 0,
-                    textStyle: {
-                            color: '#ffffff'
-                        }
-                    },
-                legend: {
-                    position: 'bottom',
-                    textStyle: {
-                            color: '#ffffff'
-                        }
-                    },
-                titleTextStyle: {
-                    color: '#ffffff'
-                }           
+            width: 630, height: 250,
+            greenFrom: 0, greenTo: 40,
+            redFrom: 70, redTo: 100,
+            yellowFrom: 40, yellowTo: 70,
+            minorTicks: 5
             };
 
-            var chart = new google.visualization.AreaChart(document.getElementById('chart_div2'));
+            var chart = new google.visualization.Gauge(document.getElementById('gauge-container'));
+
             chart.draw(data, options);
         }
         </script>
@@ -257,9 +384,17 @@ require_once("../db/connect.php");
             </header>
             <main id="dashboad-bg">
                 <div id="dashboard-canvas">
-                    <div id="chart_div" style="width: 40%; height: 500px; float: left;"></div>
-                    <div id="chart_div2" style="width: 40%; height: 500px; float: left;"></div>
+                    <div class="canvas-rows" id="top-row">
+                        <div id="gauge-container">
+
+                        </div>
                     </div>
+                    <div class="canvas-rows" id="bottom-row">
+                        <div class="charts" id="chart-1"></div>
+                        <div class="charts" id="chart-2"></div>
+                        <div class="charts" id="chart-3"></div>
+                    </div>
+                </div>
             </main>
             <footer></footer>
         </div>
