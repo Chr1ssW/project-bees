6ae725428eb5ae84b62ef2f0406b898306b67594
Marek <marek.stryjenski@gmail.com>
2020-12-18 10:22:40 +0100

    Revert "httpRead"
    This reverts commit 8254dabbe53d280ff6cd5b6c9b3ff5753db99ed5.

--- website/db/httpConnection.php
+++ website/db/httpConnection.php
@@ -8,30 +8,16 @@
 //     echo 'no';
 // }
 
-header("Content-type: application/json");
-$json = file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php');
-$obj = json_decode($json, JSON_PRETTY_PRINT);
-echo json_encode($json, JSON_PRETTY_PRINT);
-
-// foreach ($obj as $data) {
-//     //Convert object to array
-//     $device = (array)$object;
-
-//     $humid = $device["humidity"];
-//     $temp = $device["temperature"];
-//     // $weight = $device["Weight"];
-//     $device_id = $device["device_id"];
-//     $time = $device["time"];
-//     $decode = base64_decode(json_encode($obj->de));
-
-//     $fp = @fopen("TMP.txt", "a");
-//     fwrite($fp, $decoded);
-//     fwrite($fp, "\r\n");
-//     fclose($fp);
-//     //Inserts data to database
-
-//     // if ($stmtInsertDataToDb = mysqli_prepare($conn, $insertDataToDb)) {
-//     //     mysqli_stmt_bind_param($stmtInsertDataToDb, 'sddids', $device_id, $temp, $temp, $humid, $weight, $time);
-//     //     mysqli_stmt_execute($stmtInsertDataToDb);
-//     // }
+// header("Content-type: application/json");
+// $json = file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php');
+// $obj = json_decode($json);
+// foreach ($obj as $id) {
+//     echo $id;
 // }
+
+// $decode = base64_decode(json_encode($obj->data));
+
+// $fp = @fopen("TMP.txt", "a");
+// fwrite($fp, $decoded);
+// fwrite($fp, "\r\n");
+// fclose($fp);
