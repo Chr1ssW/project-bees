698986f4ae03f2f0f207f0374700749c0bf9a5f4
Marek <marek.stryjenski@gmail.com>
2020-12-18 10:13:20 +0100

    reverseHttp

--- website/db/httpConnection.php
+++ website/db/httpConnection.php
@@ -8,16 +8,16 @@
 //     echo 'no';
 // }
 
-header("Content-type: application/json");
-$json = file_get_contents("php://input");
-$obj = json_decode($json);
-foreach ($obj as $id) {
-    echo $id;
-}
+// header("Content-type: application/json");
+// $json = file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php');
+// $obj = json_decode($json);
+// foreach ($obj as $id) {
+//     echo $id;
+// }
 
-$decode = base64_decode(json_encode($obj->data));
+// $decode = base64_decode(json_encode($obj->data));
 
-$fp = @fopen("TMP.txt", "a");
-fwrite($fp, $decoded);
-fwrite($fp, "\r\n");
-fclose($fp);
+// $fp = @fopen("TMP.txt", "a");
+// fwrite($fp, $decoded);
+// fwrite($fp, "\r\n");
+// fclose($fp);
