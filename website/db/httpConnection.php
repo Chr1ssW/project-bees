74e8622c5fb6d832348ce2b1a17807cbedb4ee69
Marek <marek.stryjenski@gmail.com>
2020-12-18 10:11:14 +0100

    httpTest

--- website/db/httpConnection.php
+++ website/db/httpConnection.php
@@ -1,9 +1,23 @@
 <?php
-//Receive from TTN
-// $postdata = file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php');
-$ttndtajs = json_decode(file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php'), true);
-if ($ttndtajs) {
-    echo 'works';
-} else {
-    echo 'no';
+//// Receive from TTN
+//// $postdata = file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php');
+// $ttndtajs = json_decode(file_get_contents('https://nhl-stenden-hives.herokuapp.com/website/db/httpConnection.php'), true);
+// if ($ttndtajs) {
+//     echo 'works';
+// } else {
+//     echo 'no';
+// }
+
+header("Content-type: application/json");
+$json = file_get_contents("php://input");
+$obj = json_decode($json);
+foreach ($obj as $id) {
+    echo $id;
 }
+
+$decode = base64_decode(json_encode($obj->data));
+
+$fp = @fopen("TMP.txt", "a");
+fwrite($fp, $decoded);
+fwrite($fp, "\r\n");
+fclose($fp);
